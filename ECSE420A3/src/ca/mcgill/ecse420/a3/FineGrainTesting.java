9b7e0a7a3224e28c5092b0c2a5f020ad6f900db7
EgeAydede <ege.aydede@mail.mcgill.ca>
2017-11-22 00:52:39 -0500

    Initial contains method
    Also contains node class

---
+++ ECSE420A3/src/ca/mcgill/ecse420/a3/FineGrainTesting.java
@@ -0,0 +1,51 @@
+package ca.mcgill.ecse420.a3;
+
+public class FineGrainTesting {
+	
+	public static void main(String[] args){
+		
+		//These are in order of increasing key values
+		QNode strawberry = new QNode.QNodeBuilder("strawberry").build();
+		QNode apple = new QNode.QNodeBuilder("apple").setNext(strawberry).build();
+		QNode kiwi = new QNode.QNodeBuilder("kiwi").setNext(apple).build();
+		QNode banana = new QNode.QNodeBuilder("banana").setNext(kiwi).build();
+		
+//		QNode head = banana;
+//		while(head!=null){
+//			System.out.println("Fruit: " + head.fruit + ", Hashcode: " + head.key);
+//			head = head.next;
+//		}
+		
+		System.out.println(contains(banana, "adsf"));
+	}
+	
+	public static boolean contains(QNode head, String fruit){
+		QNode curr = head, pred = null;
+		int key = fruit.hashCode();
+		
+		if(head.next==null){
+			
+		}else{
+			curr.nodeLock.lock();
+		}
+		
+		try{
+			while(curr.key<=key){
+				if(fruit==curr.fruit){
+					return true;
+				}else if(pred!=null){
+					pred.nodeLock.unlock();
+				}
+
+				pred = curr;
+
+				curr = curr.next;
+				curr.nodeLock.lock();
+			}
+			return false;
+		}finally{	
+			pred.nodeLock.unlock();
+			curr.nodeLock.unlock();
+		}
+	}
+}
